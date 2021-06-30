030f7c227f8340957c8a6ce82d825b2abb269464
Beta-sebas <jsebas.95@hotmail.com>
2021-06-29 20:35:06 -0500

    Agregada la funcionalidad de login y gestion de usuarios

--- js/menuResponsive.js
+++ js/menuResponsive.js
@@ -1,8 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
 //Código para el botón responsivo de los botones de navegación para movil
 const menu = document.querySelector("#boton-menu");
-menu.addEventListener("click", myFunction);
+menu.addEventListener("click", responsive);
 
-function myFunction() {
+function responsive() {
   let x = document.querySelector(".nav-movil");
   if (x.className.indexOf("mostrar-menu") == -1) {
     x.className += " mostrar-menu";
@@ -10,4 +11,19 @@ function myFunction() {
     x.className = x.className.replace(" mostrar-menu", "");
   }
 }
+
+let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+let usuarioEncontrado = usuarios.find(element => element.logueado==1);
+if (usuarioEncontrado.rol == "operario") {
+  bloqueadas=document.querySelectorAll(".rol");
+  bloqueadas.forEach((item, i) => {
+    item.classList.add("d-none");
+  });
+  if (document.getElementById("botonAbastecer")) {
+    // Deshabilitar boton de abastecer al ser operario
+    document.getElementById('botonAbastecer').disabled=true;
+  }
+  
+}
 //----------------------------------------------------
+});
