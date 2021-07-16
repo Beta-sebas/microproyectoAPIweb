ce1e9e90d1b0487752b39b17943a4dcf203f1eb2
Beta-sebas <jsebas.95@hotmail.com>
2021-07-16 13:00:39 -0500

    alertas finalizadas, inicio de ordenes en proceso

--- js/operacion_salida.js
+++ js/operacion_salida.js
@@ -1,4 +1,8 @@
 let catalogoCategoriasProductos = JSON.parse(localStorage.getItem("catalogoCategoriasProductos"));
+if (localStorage.getItem("operacionesSalida")==null) {
+  let arregloVacio = [];
+  localStorage.setItem("operacionesSalida", JSON.stringify(arregloVacio));
+}
 let catalogoClientes = JSON.parse(localStorage.getItem("catalogoClientes"));
 let listaCategorias = document.getElementById("categorias");
 let listaProductos = document.getElementById("productos");
@@ -9,9 +13,17 @@ let arregloCompra = [];
 let total = 0;
 
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    llenarCategorias();
+    llenarClientes();
+
+    let clienteEscogido = listaClientes.value;
+    if(clienteEscogido==""){
+        listaCategorias.disabled = true;
+    }
+    else{
+        listaCategorias.disabled = false;
+    }
+
     let categoriaEscogida = listaCategorias.value;
     if(categoriaEscogida==""){
         listaProductos.disabled = true;
@@ -19,11 +31,42 @@ document.addEventListener("DOMContentLoaded", function () {
     else{
         listaProductos.disabled = false;
     }
-
-    llenarClientes();
 });
 
+function verificarOrdenes() {
+
+  let txtCliente = listaClientes.options[listaClientes.selectedIndex].text;
+  let ordenesTrabajo = JSON.parse(localStorage.getItem("operacionesSalida"));
+  let ordenesEnProceso = [];
+  ordenesTrabajo.forEach((ordenActual, indiceActual) => {
+    if (ordenActual.estado=="EnProceso") {
+      ordenesEnProceso.push(ordenActual);
+      console.log(ordenEnproceso.length);
+    }
+  });
+  if (ordenEnproceso.length=0) {
+    llenarCategorias();
+  }
+  else {
+    let ordenEncontrada = ordenesEnProceso.find(element=> element.cliente==txtCliente);
+    arregloCompra = ordenEncontrada.productos;
+    imprimirProductosCompra();
+    llenarCategorias();
+  }
+
+}
+
 function llenarCategorias() {
+  let clienteEscogido = listaClientes.value;
+
+  if (clienteEscogido=="") {
+    listaCategorias.disabled=true;
+    listaCategorias.innerHTML="";
+    listaCategorias.innerHTML=`<option value="" selected="selected"> - selecciona -</option>`;
+  }
+
+  else {
+    listaCategorias.disabled=false;
     listaCategorias.innerHTML="";
     listaCategorias.innerHTML=`<option value="" selected="selected"> - selecciona -</option>`;
     catalogoCategoriasProductos.forEach( (categoriaActual, indiceActual) => {
@@ -32,12 +75,11 @@ function llenarCategorias() {
         <option value="${indiceActual}">${categoriaActual.nombreCategoria}</option>
         `;
     });
+  }
 }
-
 
 function llenarProductos(){
     let categoriaEscogida = listaCategorias.value;
-    console.log(categoriaEscogida);
 
     if(categoriaEscogida==""){
         listaProductos.disabled = true;
@@ -53,6 +95,8 @@ function llenarProductos(){
         });
     }
 }
+
+
 
 function llenarClientes(){
     listaClientes.innerHTML="";
@@ -70,56 +114,104 @@ function descargarCotizacion(){
   html2pdf()
   .from(element)
   .save("cotizacion");
+  ordenEnproceso();
 }
 
+
+
+//-----------------------------------------------------Validaciones--------------------------------------
+
 function validarProducto(){
+  let categoriaEscogida = listaCategorias.value;
+  let productoEscogido = listaProductos.value;
   if (listaCategorias.value==""||listaProductos.value==""||cajaCantidad.value<=0) {
     document.getElementById("alertaProducto").classList.add("active");
   }
   else {
     document.getElementById("alertaProducto").classList.remove("active");
+    verificarCantidad()
+  }
+}
+
+function verificarCantidad(){
+  let categoriaEscogida = listaCategorias.value;
+  let productoEscogido = listaProductos.value;
+  let unidadesDisponibles = catalogoCategoriasProductos[categoriaEscogida].productos[productoEscogido].unidades;
+  if (cajaCantidad.value>unidadesDisponibles) {
+    document.getElementById("alertaCantidad").classList.add("active");
+  }
+  else {
+    document.getElementById("alertaCantidad").classList.remove("active");
     agregarProductoALaCompra();
   }
 }
 
-function validarCotizacion(){
-  if (arregloCompra.length==0) {
-    document.getElementById("alertaProducto").classList.add("active");
+function validarCantidad(){
+  let categoriaEscogida = listaCategorias.value;
+  let productoEscogido = listaProductos.value;
+  let unidadesDisponibles = catalogoCategoriasProductos[categoriaEscogida].productos[productoEscogido].unidades;
+  if (unidadesDisponibles==0) {
+    cajaCantidad.disabled=true;
+    cajaCantidad.placeholder = ` Producto Agotado `;
   }
   else {
-    document.getElementById("alertaProducto").classList.remove("active");
+    cajaCantidad.disabled=false;
+    cajaCantidad.placeholder = ` unidades disponibles : ${unidadesDisponibles}`;
+  }
+}
+
+function validarCotizacion(){
+  if (arregloCompra.length==0||listaClientes.value=="") {
+    document.getElementById("alertaCotizacion").classList.add("active");
+  }
+  else {
+    document.getElementById("alertaCotizacion").classList.remove("active");
     descargarCotizacion();
   }
 }
 
-
-
-function quitarAlerta() {
-
-  document.getElementById("alertaProducto").classList.remove("active");
+function validarPago(){
+  if (metodoPago.value=="") {
+    document.getElementById("alertaPago").classList.add("active");
+  }
+  else if (listaClientes.value=="") {
+    document.getElementById("alertaPagoCliente").classList.add("active");
+  }
+  else {
+    document.getElementById("alertaPago").classList.remove("active");
+    document.getElementById("alertaPagoCliente").classList.remove("active");
+    operacionSalida();
+  }
 }
+function quitarAlerta() {
+  document.getElementById("alertaProducto").classList.remove("active");
+  document.getElementById("alertaCantidad").classList.remove("active");
+  document.getElementById("alertaPago").classList.remove("active");
+  document.getElementById("alertaCotizacion").classList.remove("active");
+  document.getElementById("alertaPagoCliente").classList.remove("active");
+}
+
+//----------------------------------Fin------------------------------------------------------
 
 function agregarProductoALaCompra() {
     let categoriaEscogida = document.getElementById("categorias");
     let txtCategoria = categoriaEscogida.options[categoriaEscogida.selectedIndex].text;
     let productoEscogido = document.getElementById("productos");
     let txtProducto = productoEscogido.options[productoEscogido.selectedIndex].text;
-    let cantidad = parseInt(document.getElementById("cantidad").value,10);
+    let cantidad = parseInt(document.getElementById("cantidad").value,10); // se toma el valor y se convierte en unidades decimales con el 10
 
     let compraIndividual = {
         categoria:txtCategoria,
         producto:txtProducto,
         cantidad:cantidad
     };
-
     arregloCompra.push(compraIndividual);
     categoriaEscogida.value="";
     llenarProductos();
-    cajaCantidad.value=0;
+    cajaCantidad.value="";
+    cajaCantidad.placeholder = "Ingrese La Cantidad ";
     imprimirProductosCompra();
-
 }
-
 
 function imprimirProductosCompra() {
   let catalogoCategoriasProductos = JSON.parse(localStorage.getItem("catalogoCategoriasProductos"));
@@ -131,7 +223,7 @@ function imprimirProductosCompra() {
       let productoEncontrado = categoriaEncontrada.productos.find(element => element.nombre==compraIndividualActual.producto);
       let precioIndividual = productoEncontrado.precioEntradas;
       let precioTotal = precioIndividual*compraIndividualActual.cantidad;
-      
+
       total+= precioTotal;
         txt+=
         `
@@ -172,20 +264,6 @@ function eliminarProducto (indice){
   imprimirProductosCompra();
 }
 
-function validarCliente(){
-  if (listaClientes.value==""||metodoPago.value=="") {
-    document.getElementById("alertaCliente").classList.add("active");
-  }
-  else {
-    document.getElementById("alertaCliente").classList.remove("active");
-    operacionSalida();
-  }
-}
-
-function quitarAlertaCliente(){
-  document.getElementById("alertaCliente").classList.remove("active");
-}
-
 function operacionSalida(){
   let d = new Date();
   let n = d.toLocaleString();
@@ -196,7 +274,8 @@ function operacionSalida(){
     total:0,
     fecha:"",
     metodoPago:"",
-    responsable:""
+    responsable:"",
+    estado:""
   };
 
   let clienteEscogido = document.getElementById("clientes");
@@ -211,18 +290,9 @@ function operacionSalida(){
   ventaFinal.fecha = n;
   ventaFinal.metodoPago = txtMetodoPago;
   ventaFinal.responsable = usuarioEncontrado.usuario;
-  
-
-  if (localStorage.getItem("operacionesSalida")==null) {
-    let arregloVacio = [];
-    localStorage.setItem("operacionesSalida", JSON.stringify(arregloVacio));
-    ventaFinal.numeroVenta = 1;
-  }
-  else {
-    let numeroDeOpercaion = JSON.parse(localStorage.getItem("operacionesSalida"));
-    ventaFinal.numeroVenta = numeroDeOpercaion.length+1;
-  }
-
+  ventaFinal.estado = "Terminado";
+  let numeroDeOpercaion = JSON.parse(localStorage.getItem("operacionesSalida"));
+  ventaFinal.numeroVenta = numeroDeOpercaion.length+1;
   let operacionesSalida = JSON.parse(localStorage.getItem("operacionesSalida"));
   operacionesSalida.push(ventaFinal);
   localStorage.setItem("operacionesSalida", JSON.stringify(operacionesSalida));
@@ -233,6 +303,42 @@ function operacionSalida(){
     let productoEncontrado = categoriaEncontrada.productos.find(element => element.nombre==compraIndividualActual.producto);
     productoEncontrado.unidades-= compraIndividualActual.cantidad;
   });
-  localStorage.setItem("catalogoCategoriasProductos", JSON.stringify(catalogoCategoriasProductos)); 
+  localStorage.setItem("catalogoCategoriasProductos", JSON.stringify(catalogoCategoriasProductos));
   window.location.href = "home.html";
+}
+
+function ordenEnproceso(){
+  let d = new Date();
+  let n = d.toLocaleString();
+  let ventaFinal = {
+    numeroVenta:0,
+    cliente:"",
+    productos:[],
+    total:0,
+    fecha:"",
+    metodoPago:"",
+    responsable:"",
+    estado:""
+  };
+
+  let clienteEscogido = document.getElementById("clientes");
+  let txtCliente = clienteEscogido.options[clienteEscogido.selectedIndex].text;
+  let metodoPago = document.getElementById("metodopago");
+  let txtMetodoPago = metodoPago.options[metodoPago.selectedIndex].text;
+  let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  let usuarioEncontrado = usuarios.find(element => element.logueado==1);
+  ventaFinal.cliente = txtCliente;
+  ventaFinal.productos = arregloCompra;
+  ventaFinal.total = total;
+  ventaFinal.fecha = n;
+  ventaFinal.metodoPago = txtMetodoPago;
+  ventaFinal.responsable = usuarioEncontrado.usuario;
+  ventaFinal.estado = "EnProceso";
+
+  let numeroDeOpercaion = JSON.parse(localStorage.getItem("operacionesSalida"));
+  ventaFinal.numeroVenta = numeroDeOpercaion.length+1;
+  let operacionesSalida = JSON.parse(localStorage.getItem("operacionesSalida"));
+  operacionesSalida.push(ventaFinal);
+  localStorage.setItem("operacionesSalida", JSON.stringify(operacionesSalida));
+
 }
